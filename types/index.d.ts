aadec33bdde12ce8ff1333a5e06d782a76eaca5b
Michael Czechowski <mail@dailysh.it>
2018-10-24 18:06:33 +0200

    extended type definition

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,8 +1,12 @@
 export interface DirecTree {
-  read: function;
-  write: function;
-  search: function;
+  read: ReadFunction;
+  search: SearchFunction;
+  write: WriteFunction;
 }
+
+export function ReadFunction(): any
+export function SearchFunction(collection: array, query: null|string): null|arry
+export function WriteFunction(): any
 
 declare const directree: DirecTree;
 
